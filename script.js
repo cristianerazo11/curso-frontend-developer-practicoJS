92626a678457dac55b526925e906145704d9a447
Cristian Erazo <cristianerazoiam11@gmail.com>
2022-09-28 01:44:09 -0500

    unimos la clase8 al index en donde organizamos el menu- mobile y añadimos media quiery para que sea responsivo

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuHambur = document.querySelector('.menuHambur');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuHambur.addEventListener('click', toggleMobileMenu);
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
-    
+
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
